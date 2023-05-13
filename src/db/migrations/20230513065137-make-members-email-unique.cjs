d51b2812900d548f4ee13a715e0d74792ec3654d
Ryan Ali <ryanali456@gmail.com>
2023-05-13 15:10:02 +0800

    add unique index to member_account email

---
+++ src/db/migrations/20230513065137-make-members-email-unique.cjs
@@ -0,0 +1,12 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.addIndex("member_account", ["email"], {
+      unique: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {},
+};
